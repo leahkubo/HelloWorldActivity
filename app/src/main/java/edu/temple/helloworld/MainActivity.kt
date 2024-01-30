250b793c2358e8c5a1955d7e789f9b0f1ca58cd1
leahkubo <kubalaleah@gmail.com>
2024-01-30 11:58:45 -0500

    Declared properties to reference all View items

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -10,6 +10,8 @@ class MainActivity : AppCompatActivity() {
 
     // Declare view properties - the first one is done for you
     lateinit var displayTextView: TextView
+    lateinit var clickMeButton: Button
+    lateinit var nameEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +19,10 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
+        clickMeButton = findViewById(R.id.clickMeButton)
+        nameEditText = findViewById(R.id.nameEditText)
+
+
 
         
         // Respond to button click event per specifications
